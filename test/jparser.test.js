7463ebe9a189fd1367a38bfb33447d90df6207d9
Dave Newton <dave.newton@littlebits.com>
2018-09-26 11:41:37 -0400

    spike/endianness Initial branch; removed write test (not implemented)

--- test/jparser.test.js
+++ test/jparser.test.js
@@ -8,125 +8,125 @@ const parser = new jParser(view)
 
 const chr = x => String.fromCharCode(x)
 
-describe('Writing', () => {
-	const tempBuffer = new Buffer(128)
-	const tempView = new jDataView(tempBuffer, 1, undefined, true)
-	const tempParser = new jParser(tempView)
+// describe('Writing', () => {
+//   const tempBuffer = new Buffer(128)
+//   const tempView = new jDataView(tempBuffer, 1, undefined, true)
+//   const tempParser = new jParser(tempView)
 
-	const testStruct = {
-		type:      2,
-		subType:   6,
-		id:        8,
-		numInputs: 2,
-		snaps:     6,
-	}
+//   const testStruct = {
+//     type:      2,
+//     subType:   6,
+//     id:        8,
+//     numInputs: 2,
+//     snaps:     6,
+//   }
 
-	const input = {
-		type:      2,
-		subType:   8,
-		id:        60,
-		numInputs: 1,
-		snaps:     18,
-	}
+//   const input = {
+//     type:      2,
+//     subType:   8,
+//     id:        60,
+//     numInputs: 1,
+//     snaps:     18,
+//   }
 
-	test('Write object', () => {
-		tempParser.seek(0)
-		tempParser.parse(testStruct, input)
-		expect(tempBuffer.slice(1,4)).toEqual([136, 60, 82])
-	})
-})
+//   test('Write object', () => {
+//     tempParser.seek(0)
+//     tempParser.parse(testStruct, input)
+//     expect(tempBuffer.slice(1,4)).toEqual([136, 60, 82])
+//   })
+// })
 
 describe('Values', () => {
-	test('uint', () => {
-		parser.seek(0)
+  test('uint', () => {
+    parser.seek(0)
 
-		expect(parser.parse('uint8')).toBe(255)
-		expect(parser.parse('uint16')).toBe(65022)
-		expect(parser.parse('uint32')).toBe(3120626428)
-	})
+    expect(parser.parse('uint8')).toBe(255)
+    expect(parser.parse('uint16')).toBe(65022)
+    expect(parser.parse('uint32')).toBe(3120626428)
+  })
 
-	test('int', () => {
-		parser.seek(0)
+  test('int', () => {
+    parser.seek(0)
 
-		expect(parser.parse('int8')).toBe(-1)
-		expect(parser.parse('int16')).toBe(-514)
-		expect(parser.parse('int32')).toBe(-1174340868)
-	})
+    expect(parser.parse('int8')).toBe(-1)
+    expect(parser.parse('int16')).toBe(-514)
+    expect(parser.parse('int32')).toBe(-1174340868)
+  })
 
-	test('float', () => {
-		parser.seek(0)
-		expect(parser.parse('float32')).toBe(-1.055058432344064e+37)
+  test('float', () => {
+    parser.seek(0)
+    expect(parser.parse('float32')).toBe(-1.055058432344064e+37)
 
-		parser.seek(0)
-		expect(parser.parse('float64')).toBe(2.426842827241402e-300)
-	})
+    parser.seek(0)
+    expect(parser.parse('float64')).toBe(2.426842827241402e-300)
+  })
 
-	test('string', () => {
-		parser.seek(5)
+  test('string', () => {
+    parser.seek(5)
 
-		expect(parser.parse('char')).toBe(chr(0x00))
-		expect(parser.parse(['string', 2])).toBe(chr(0xba) + chr(0x01))
-	})
+    expect(parser.parse('char')).toBe(chr(0x00))
+    expect(parser.parse(['string', 2])).toBe(chr(0xba) + chr(0x01))
+  })
 
-	test('array', () => {
-		parser.seek(0)
-		expect(parser.parse(['array', 'uint8', 8])).toEqual([0xff, 0xfe, 0xfd, 0xfc, 0xfa, 0x00, 0xba, 0x01])
+  test('array', () => {
+    parser.seek(0)
+    expect(parser.parse(['array', 'uint8', 8])).toEqual([0xff, 0xfe, 0xfd, 0xfc, 0xfa, 0x00, 0xba, 0x01])
 
-		parser.seek(0)
-		expect(parser.parse(['array', 'int32', 2])).toEqual([-50462977, 28967162])
-	})
+    parser.seek(0)
+    expect(parser.parse(['array', 'int32', 2])).toEqual([-50462977, 28967162])
+  })
 
-	test('object', () => {
-		parser.seek(0)
+  test('object', () => {
+    parser.seek(0)
 
-		expect(parser.parse({
-			a: 'int32',
-			b: 'int8',
-			c: ['array', 'uint8', 2]
-		})).toEqual({
-			a: -50462977,
-			b: -6,
-			c: [0, 186]
-		})
-	})
+    expect(parser.parse({
+      a: 'int32',
+      b: 'int8',
+      c: ['array', 'uint8', 2]
+    })).toEqual({
+      a: -50462977,
+      b: -6,
+      c: [0, 186]
+    })
+  })
 
-	test('bitfield', () => {
-		parser.seek(6)
+  test('bitfield', () => {
+    parser.seek(6)
 
-		expect(parser.parse({
-			first5: 5,
-			next5: function () {
-				return this.parse(5)
-			},
-			last6: {
-				first3: 3,
-				last3: 3
-			}
-		})).toEqual({
-			first5: 0x17,
-			next5: 0x08,
-			last6: {
-				first3: 0,
-				last3: 1
-			}
-		})
-	})
+    expect(parser.parse({
+      first5: 5,
+      next5: function () {
+        return this.parse(5)
+      },
+      last6: {
+        first3: 3,
+        last3: 3
+      }
+    })).toEqual({
+      first5: 0x17,
+      next5: 0x08,
+      last6: {
+        first3: 0,
+        last3: 1
+      }
+    })
+  })
 })
 
 describe('Utils', () => {
-	test('seek', () => {
-		parser.seek(5)
-		expect(parser.tell()).toBe(5)
+  test('seek', () => {
+    parser.seek(5)
+    expect(parser.tell()).toBe(5)
 
-		parser.seek(parser.tell() - 2)
-		expect(parser.tell()).toBe(3)
+    parser.seek(parser.tell() - 2)
+    expect(parser.tell()).toBe(3)
 
-		parser.seek(5, () => {
-			expect(parser.tell()).toBe(5)
-			parser.seek(0)
-			expect(parser.tell()).toBe(0)
-		})
+    parser.seek(5, () => {
+      expect(parser.tell()).toBe(5)
+      parser.seek(0)
+      expect(parser.tell()).toBe(0)
+    })
 
-		expect(parser.tell()).toBe(3)
-	})
+    expect(parser.tell()).toBe(3)
+  })
 })
